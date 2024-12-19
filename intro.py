43034ee453b17dbd99c3c0c288d334fd3041320f
Adarsh B <66517366+Adarshxox@users.noreply.github.com>
2024-12-19 22:06:51 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,30 @@
+# name = "adarsh"
+
+# print("Hello" ,name)
+
+company_name = "Luminar"
+
+place = "Kochi"
+
+course = "Full Stack"
+
+duration = 6
+
+benefit = "app development"
+
+# Hi Adarsh you are doing Full stack in Luminar at Kochi
+
+print("Hi Adarsh you are doing" , course, "in", company_name, "at" , place)
+
+# f- string
+
+print(f"{course} its duration is {duration} months in {benefit}")
+
+# {python} is object oriented language , used to create {application} backend, and apis
+
+course2 = "Python"
+
+benefit2 = "application"
+
+print(f"{course2} is object oriented language , used to create {benefit2} backend ,and APIs")
+
---
+++ sample.py
@@ -0,0 +1,9 @@
+print("hello_world")
+
+# to print the "hello_world"
+
+print("welcome to the world of coding")
+
+# to "print" the world in console
+
+print("welcome to python")
