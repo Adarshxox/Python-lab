ea48a0ef2b782bd9b2f33e966c188779cd6799af
Adarsh B <66517366+Adarshxox@users.noreply.github.com>
2024-12-27 10:56:49 +0530

    Add files via upload
    calculation using python program

---
+++ calculator.py
@@ -0,0 +1,46 @@
+# basic calculator
+# ================
+
+# addition
+
+# subtraction
+
+# division
+
+# multiplication
+
+# modulus
+
+
+# a = 5
+
+# b = 10
+
+# the addition result of 5 and 10 is 15
+
+# the subtraction result of 5 and 10 is -5
+
+a = 33
+
+b = 21
+
+result = a + b 
+
+print(f"the addition result of {a} and {b} is {result}")
+
+subtraction = a - b
+
+print(f"the subtraction result of {a} and {b} is {subtraction}")
+
+division = a / b
+
+print(f"the division result of {a} and {b} is {division}")
+
+multiplication = a * b
+
+print(f"the multiplication result of {a} and {b} is {multiplication}")
+
+modulus = a % b
+
+print(f"the modulus result of {a} and {b} is {modulus}")
+
