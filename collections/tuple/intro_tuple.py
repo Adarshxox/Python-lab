a3a7ffa336ec64c6a1b20095a31e5aa618cbde07
Adarsh <66517366+Adarshxox@users.noreply.github.com>
2025-01-21 21:24:58 +0530

    Add files via upload

---
+++ collections/tuple/intro_tuple.py
@@ -0,0 +1,14 @@
+# items = (1,2,3,4,4,2,6,True)
+
+# print(items.count(2))
+
+# print(items.index(3))
+
+# elements= ("hello",2,["Hai",3,True])
+
+# print(elements)
+
+# print(elements[2])
+
+# print(elements[2][2])
+
---
+++ collections/tuple/sample.py
@@ -0,0 +1,15 @@
+elements= ("hello",2,["Hai",3,True])  #("hello",2,["Hai",3,True,"python"])
+
+# elements[2].append("python")
+
+# print(elements)
+
+# elements[2].insert(1,"python")
+
+# print(elements)
+
+#("hello",2,[(1,2,3),"Hai",3,True])
+
+elements[2].insert(0,("1,2,3"))
+
+print(elements)
