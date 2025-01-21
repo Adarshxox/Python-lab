723ff6d22d5a8c344e724d65476883c9da3c8343
Adarsh <66517366+Adarshxox@users.noreply.github.com>
2025-01-21 21:24:07 +0530

    Add files via upload

---
+++ collections/set/intro_set.py
@@ -0,0 +1,49 @@
+# items = {1,False,2,3,4,True,1,2,0}
+
+# print(items)
+
+
+
+# items ={1,2,3,4,2,"hai",1,6,5}
+
+# print(list(set(items)))
+
+# items.add()                           # Add an element to a set.
+
+# items.clear()
+
+# items.pop()
+
+# items.remove()                        # Remove an element from a set; it must be a member.
+
+# set1 = {1,2,3,4,5,6}
+
+# set2 = {5,6,7,8,9,10}
+
+# print(set1.union(set2))                # Return the union of sets as a new set.
+
+# print(set1.intersection(set2))         # Return the intersection of two sets as a new set.
+
+# print(set1.difference(set2))           # Return the difference of two or more sets as a new set.
+
+# print(set2.difference(set1))
+
+# set1 = {1,2,3,4,5,6}
+
+# set2 = {5,6,4,7,8,9,10}
+
+# print(set1.issuperset(set2))            # Test whether every element in other is in the set.
+
+# print(set2.issubset(set1))
+
+set1 = {"apple","orange","pineapple"}
+
+set2 = {"microsoft","google","apple"}
+
+print(set1.intersection(set2))
+
+print(set1.intersection_update(set2))
+
+# print(set1.difference(set2))   
+
+# print(set1.difference_update(set2))
