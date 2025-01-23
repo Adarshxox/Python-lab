f1c93798d1a8ff6edeef32848055083f4e67885d
Adarsh <66517366+Adarshxox@users.noreply.github.com>
2025-01-23 11:08:53 +0530

    Delete task/9-1-2025 directory

--- task/9-1-2025/task_050.py
+++
@@ -1,9 +0,0 @@
-"""""
-
-Ask the user to enter the number between 10 and 20. If they enter a value under 10, Display the message "too low" and ask them to try again.
-If they enter a value above 20, Display the message "too high" and ask them to try again. Keep repeating this until they enter a value that 
-is
-
-
-"""
-
