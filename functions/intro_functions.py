fcda1233e9397c32ae07f33a03c64b5c80135e07
Adarsh <66517366+Adarshxox@users.noreply.github.com>
2025-01-27 11:07:32 +0530

    Add files via upload

---
+++ functions/intro_functions.py
@@ -0,0 +1,37 @@
+#  Different types of functions
+#  ============================
+
+#  print()
+
+#  input()                 user input str
+
+#  len()                   str list
+
+#  range()                 sequence of numbers
+
+"----------------------------------------------------------------------------------------------------------------------------------------"
+
+# to print hello world .
+" ******************** "
+
+# def used to define a function
+
+# def functionname():      function header
+
+#       statement          function body
+
+# function name()          calling function
+
+def greet():
+
+    print("hello world")
+
+greet()
+
+
+
+" ADVANTAGES "
+"------------"
+
+# reuse
+
