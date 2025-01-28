5b77b74e49535d6b80fc58c294bd7a7ec3d9bd8a
Adarsh <66517366+Adarshxox@users.noreply.github.com>
2025-01-28 10:01:15 +0530

    Update kangaroo.py
    Full code for kangaroo program

--- collections/dicitonary/kangaroo.py
+++ collections/dicitonary/kangaroo.py
@@ -1,2 +1,29 @@
 text = "regulate"
 
+word = input("enter the word: ")
+
+# kangaroo >> chars should be in the source word
+
+w_count = {}
+
+for i in text:
+
+    w_count[i] = text.count(i)
+
+print(w_count)
+
+for j in word:
+
+    if j in w_count and w_count[j] > 0:
+
+        w_count[j] -= 1
+
+    else:
+
+        print("Not kangaroo")
+
+        break
+
+else:
+
+    print("Is a Kangaroo")
