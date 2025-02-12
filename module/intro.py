0c5f13a7e474efc6db2086b9f5a75dd1565385d3
Adarsh <66517366+Adarshxox@users.noreply.github.com>
2025-02-12 08:53:29 +0530

    Add files via upload

---
+++ module/intro.py
@@ -0,0 +1,8 @@
+# modules & packages
+# ==================
+
+
+# from functions import sum_numbers   # in this line we call a modeule from one folder from that folder to another file
+
+# sum_numbers.sum(12)
+
